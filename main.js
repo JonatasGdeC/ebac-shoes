8e551c3317a0a4f40bd9825a883717ba449db83e
Jonatas <jonatas.07022003@gmail.com>
2023-04-28 22:37:19 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('#carrossel-imagens').slick({
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true
+    })
+
+    $('#todos-produtos').slick({
+
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+            breakpoint: 1112,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            }
+            },
+        {
+            breakpoint: 1023,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 640,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+})
